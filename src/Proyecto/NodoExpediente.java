92bf5ea1f4ae7f9143ccf9fe6b0a87c075fb86c4
AngelRojas05 <jesusrojas123424@gmail.com>
2025-06-29 18:44:33 -0500

    Create NodoExpediente.java

---
+++ src/Proyecto/NodoExpediente.java
@@ -0,0 +1,10 @@
+public class NodoExpediente {
+    Expediente dato;
+    NodoExpediente siguiente;
+
+    public NodoExpediente(Expediente dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+    
+}
